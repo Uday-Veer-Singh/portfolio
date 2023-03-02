a67a1ea00569013e748b24c684459465fcb9f0d6
U-Dev <udaythakur.shely@gmail.com>
2023-03-01 19:37:17 -0600

    slider section

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,13 +1,47 @@
-// Add an "active" class to the current nav link
-const header = document.getElementById("header");
-const links = header.getElementsByTagName("a");
+const slides = document.querySelectorAll(".slide");
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
+const intervalTime = 5000;
+let slideInterval;
 
-// for (let i = 0; i < links.length; i++) {
-//   links[i].addEventListener("click", function () {
-//     let current = document.getElementsByClassName("active");
-//     if (current.length > 0) {
-//       current[0].className = current[0].className.replace(" active", "");
-//     }
-//     this.className += " active";
-//   });
-// }
+next.addEventListener("click", (e) => {
+  nextSlide();
+  if (true) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+});
+
+prev.addEventListener("click", (e) => {
+  prevSlide();
+  if (true) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+});
+
+const nextSlide = () => {
+  const current = document.querySelector(".current");
+  current.classList.remove("current");
+  if (current.nextElementSibling) {
+    current.nextElementSibling.classList.add("current");
+  } else {
+    slides[0].classList.add("current");
+  }
+  setTimeout(() => current.classList.remove("current"));
+};
+
+const prevSlide = () => {
+  const current = document.querySelector(".current");
+  current.classList.remove("current");
+  if (current.previousElementSibling) {
+    current.previousElementSibling.classList.add("current");
+  } else {
+    slides[slides.length - 1].classList.add("current");
+  }
+  setTimeout(() => current.classList.remove("current"));
+};
+
+if (auto) {
+  slideInterval = setInterval(nextSlide, intervalTime);
+}
